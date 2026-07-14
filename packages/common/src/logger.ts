a54d7e1a748f572d20c1e9bb32b30925dd72084c
Bashbuilder <iamadelowotan@gmail.com>
2026-07-14 18:41:10 +0100

    feat: initialized a workspace

---
+++ packages/common/src/logger.ts
@@ -0,0 +1,28 @@
+import type { Logger, LoggerOptions } from "pino";
+import pino from "pino";
+
+type CreateLoggerOptions = LoggerOptions & {
+  name: string;
+};
+
+export const createLogger = (options: CreateLoggerOptions): Logger => {
+  const { name, ...rest } = options;
+
+  const transport =
+    process.env.NODE_ENV === "development"
+      ? {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "SYS:standard",
+          },
+        }
+      : undefined;
+
+  return pino({
+    name,
+    level: process.env.LOG_LEVEL || "info",
+    transport,
+    ...rest,
+  });
+};
