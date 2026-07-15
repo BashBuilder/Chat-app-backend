519309ec41f08aea00c0bd1be4846d09d990f3c9
Bashbuilder <iamadelowotan@gmail.com>
2026-07-15 13:03:44 +0100

    feat: new modules configuration updated

--- services/auth-service/src/app.ts
+++ services/auth-service/src/app.ts
@@ -1,7 +1,27 @@
 import express, { type Application } from "express";
+import helmet from "helmet";
+import cors from "cors";
+import { errorHandler } from "@/middlewares/error-handler";
 
 export const createApp = (): Application => {
   const app = express();
+
+  app.use(helmet());
+  app.use(
+    cors({
+      origin: "*",
+      credentials: true,
+    }),
+  );
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use((_, res) => {
+    res.status(400).json({ message: "Not found" });
+  });
+
+  app.use(errorHandler);
 
   return app;
 };
