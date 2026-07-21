91e17c474c561883da53fa5077504f6755cad2b3
Bashbuilder <iamadelowotan@gmail.com>
2026-07-21 20:14:15 +0100

    feat: added auth dockerfile

--- services/auth-service/src/routes/index.ts
+++ services/auth-service/src/routes/index.ts
@@ -2,5 +2,9 @@ import { Router } from 'express';
 import { authRouter } from '@/routes/auth.route';
 
 export const registerRoutes = (app: Router) => {
+  // health
+  app.get('/health', (_, res) => {
+    res.json({ status: 'Healthy', services: 'auth-service' });
+  });
   app.use('/auth', authRouter);
 };
